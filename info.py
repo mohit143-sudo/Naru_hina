c367c46c17435db6602ff949a11b393676ffc655
Detailmovie0 <moviehub4uofficial0@gmail.com>
2025-09-09 23:13:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -56,7 +56,7 @@ CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Decodingtm4mohit')
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/hinkidi')
 UPDATE_CHANNEL_LNK = environ.get('UPDATE_CHANNEL_LNK', 'https://t.me/Decodingtm4mohit')
 
-AUTH_CHANNEL = environ.get("-1002583222762") # add multiple channels here, separated by single space
+AUTH_CHANNEL = environ.get("-1002583222762 -1002908463240") # add multiple channels here, separated by single space
 AUTH_REQ_CHANNEL = environ.get('AUTH_REQ_CHANNEL', '-1002683567019') # add multiple channels here, separated by single space
 
 IS_VERIFY = is_enabled('IS_VERIFY', False)
